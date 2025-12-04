398e9b95c0316d065468b59ac3e74e21f0b5988d
prakashsingh62 <prakashkumarsing@gmail.com>
2025-12-04 12:43:38 +0530

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -1,31 +1,16 @@
-import os
-from sendgrid import SendGridAPIClient
+import sendgrid
 from sendgrid.helpers.mail import Mail
-from config import SENDGRID_API_KEY, SENDGRID_FROM_EMAIL, EMAIL_RECIPIENTS
-from logger import get_logger
+from config import SENDGRID_API_KEY, SENDGRID_FROM_EMAIL
 
-logger = get_logger()
+def send_email(to_emails, subject, html_content):
+    sg = sendgrid.SendGridAPIClient(api_key=SENDGRID_API_KEY)
 
-def send_email(subject, html_body):
-    recipients = [x.strip() for x in EMAIL_RECIPIENTS.split(",") if x.strip()]
-
-    if not recipients:
-        logger.error("NO EMAIL RECIPIENTS FOUND — Email not sent.")
-        return False
-
-    message = Mail(
+    email = Mail(
         from_email=SENDGRID_FROM_EMAIL,
-        to_emails=recipients,
+        to_emails=to_emails,
         subject=subject,
-        html_content=html_body,
+        html_content=html_content
     )
 
-    try:
-        sg = SendGridAPIClient(SENDGRID_API_KEY)
-        response = sg.send(message)
-        logger.info(f"Email sent → Status Code: {response.status_code}")
-        return True
-
-    except Exception as e:
-        logger.error(f"send_email() failure: {str(e)}")
-        return False
+    response = sg.send(email)
+    return response.status_code
