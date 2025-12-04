270daac32a11fbec92886e9debb5de969c597dad
prakashsingh62 <prakashkumarsing@gmail.com>
2025-12-04 12:33:07 +0530

    Update router_email.py

--- router_email.py
+++ router_email.py
@@ -1,9 +1,22 @@
 from fastapi import APIRouter
-from email_sender import send_email
+from sheet_reader import read_sheet
+from logic_engine import prepare_rows_for_email
+from email_builder import build_email_html
 
 router = APIRouter()
 
+
 @router.post("/email")
-def email_api():
-    result = send_email()
-    return {"status": "email_sent", "response": result}
+def email_preview():
+    """
+    Returns final formatted HTML email WITHOUT sending it.
+    """
+    rows = read_sheet()
+    summary, sections = prepare_rows_for_email(rows)
+
+    html = build_email_html(summary, sections)
+
+    return {
+        "status": "email_preview_generated",
+        "html": html
+    }
