9045d7467883a96b5c47ded88b400ebd0dbeaf1d
prakashsingh62 <prakashkumarsing@gmail.com>
2025-12-04 15:25:18 +0530

    Update router_email.py

--- router_email.py
+++ router_email.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter
-from email_sender import send_email
-from email_builder import build_email_html
+from email_sender import send_email          # same folder import
+from email_builder import build_email_html   # same folder import
 
 router = APIRouter()
 
 @router.post("/email")
 async def send_email_route():
     """
-    Manual email trigger (for debugging)
+    Manual email trigger (debugging)
     """
     html = build_email_html([])
     send_email(html)
