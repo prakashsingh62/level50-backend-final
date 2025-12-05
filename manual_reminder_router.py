1571224c6080bc303e30df183cb1fac983a391ce
prakashsingh62 <prakashkumarsing@gmail.com>
2025-12-05 20:58:25 +0530

    Update manual_reminder_router.py

--- manual_reminder_router.py
+++ manual_reminder_router.py
@@ -4,6 +4,9 @@ from daily_sender import send_manual_reminder
 router = APIRouter()
 
 @router.post("/manual-reminder")
-async def manual_reminder():
+def manual_reminder():
     result = send_manual_reminder()
-    return {"status": "manual reminder sent", "send_result": result}
+    return {
+        "status": "manual reminder triggered",
+        "result": result
+    }
