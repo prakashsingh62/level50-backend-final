2c7a47fe2c0f309f9ae43d65fc7a339b1693809d
prakashsingh62 <prakashkumarsing@gmail.com>
2025-12-22 22:37:09 +0530

    Create json_safe.py

---
+++ utils/json_safe.py
@@ -0,0 +1,10 @@
+from datetime import datetime, date
+
+def json_safe(obj):
+    if isinstance(obj, dict):
+        return {k: json_safe(v) for k, v in obj.items()}
+    if isinstance(obj, list):
+        return [json_safe(v) for v in obj]
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    return obj
