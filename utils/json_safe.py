5d4695cf1e8e91e39e6f919c91a51e201691260b
prakashsingh62 <prakashkumarsing@gmail.com>
2025-12-24 12:38:29 +0530

    Update json_safe.py

--- utils/json_safe.py
+++ utils/json_safe.py
@@ -1,10 +1,11 @@
+import json
 from datetime import datetime, date
 
+
 def json_safe(obj):
-    if isinstance(obj, dict):
-        return {k: json_safe(v) for k, v in obj.items()}
-    if isinstance(obj, list):
-        return [json_safe(v) for v in obj]
-    if isinstance(obj, (datetime, date)):
-        return obj.isoformat()
-    return obj
+    def default(o):
+        if isinstance(o, (datetime, date)):
+            return o.isoformat()
+        return str(o)
+
+    return json.loads(json.dumps(obj, default=default))
