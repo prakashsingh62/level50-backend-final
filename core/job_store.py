be79dbf04324ecc7e127c0f6e4e28ecd3dfb1139
prakashsingh62 <prakashkumarsing@gmail.com>
2025-12-24 15:01:48 +0530

    Update job_store.py

--- core/job_store.py
+++ core/job_store.py
@@ -1,1 +1,18 @@
+# core/job_store.py
+from typing import Dict
+
+JOB_STATUS: Dict[str, dict] = {}
+
+def create_job(trace_id: str):
+    JOB_STATUS[trace_id] = {
+        "status": "QUEUED",
+        "processed": 0,
+        "total": 0
+    }
+
+def update_job(trace_id: str, **kwargs):
+    JOB_STATUS[trace_id].update(kwargs)
+
+def get_job(trace_id: str):
+    return JOB_STATUS.get(trace_id)
 
