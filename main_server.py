b8ee6c5a9108987e985f3fec47d4a0659ea72468
prakashsingh62 <prakashkumarsing@gmail.com>
2026-01-01 17:47:13 +0530

    Update main_server.py

--- main_server.py
+++ main_server.py
@@ -5,25 +5,23 @@ import logic_engine
 
 app = FastAPI()
 
-# Ye model zaroori hai taaki server tera JSON Body padh sake
 class AutomationRequest(BaseModel):
     spreadsheet_id: str
     sheet_name: str = "Production"
 
 @app.get("/")
 async def root():
-    return {"message": "System Online - Ready for Phase 17"}
+    return {"message": "Level 80 System Online"}
 
 @app.post("/automation/run")
 async def trigger_run(request: AutomationRequest, background_tasks: BackgroundTasks, debug: bool = Query(False)):
-    # Yahan hum logic_engine ke naye function ko data bhej rahe hain
     background_tasks.add_task(
         logic_engine.run_level50, 
         spreadsheet_id=request.spreadsheet_id, 
         sheet_name=request.sheet_name,
         debug=debug
     )
-    return {"status": "Started", "info": "Level 80 scanning in background."}
+    return {"status": "Started", "info": "Scanning in background"}
 
 if __name__ == "__main__":
     import uvicorn
